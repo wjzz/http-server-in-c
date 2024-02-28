8db0fe0b4b6afb1617481f232d0173c3ccc2b347
Wojciech Jedynak <wjedynak@gmail.com>
2024-02-28 12:36:42 +0900

    Finished CPP single threaded versions

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,5 @@
-#include <cstdio>
-#include <cstdlib>
-#include <cstring>
 #include <iostream>
-#include <netinet/in.h>
 #include <string>
-#include <sys/socket.h>
 #include <unistd.h>
 
 #include "server.h"
@@ -18,17 +13,24 @@ struct request_t {
   std::string path;
 };
 
-request_t parse_request(const std::string &input) {
+std::tuple<std::string, std::string> extract_path(const std::string &input) {
   size_t pos = input.find_first_of('\n');
   std::string first_line = input.substr(0, pos);
   std::cout << first_line << std::endl;
 
   size_t space_pos = first_line.find_first_of(' ');
   std::string method = first_line.substr(0, space_pos);
+
   std::string remaining = first_line.substr(1 + space_pos);
   size_t second_space_pos = remaining.find_first_of(' ');
+
   std::string path = remaining.substr(
       1, second_space_pos - 1); // remove the / at the beginning
+  return std::tuple(method, path);
+}
+
+request_t parse_request(const std::string &input) {
+  auto [method, path] = extract_path(input);
 
   if (path == "") {
     path = "index.html";
@@ -38,20 +40,28 @@ request_t parse_request(const std::string &input) {
   return request;
 }
 
-void handle_connection(int socket) {
-  // TODO: maybe we don't read everything here
+std::string read_all(int socket) {
+  std::string request_buffer;
   char buffer[BUFSIZE] = {0};
+  // int n;
+  // while ((n = read(socket, buffer, BUFSIZE - 1)) > 0) {
+  //   request_buffer += std::string(buffer, buffer + n);
+  // }
   read(socket, buffer, BUFSIZE - 1);
+  request_buffer = buffer;
+  return request_buffer;
+}
 
-  //   printf("Got message %s\n", buffer);
+void handle_connection(int socket) {
+  auto request_buffer = read_all(socket);
 
-  request_t request = parse_request(buffer);
-  printf("Got path = %s\n", request.path.c_str());
+  request_t request = parse_request(request_buffer);
+  std::cout << "Got path = " << request.path << std::endl;
 
   std::string payload = access_file(request.path);
 
   send(socket, payload.c_str(), payload.length(), 0);
-  printf("Reply OK.\n");
+  std::cout << "Reply OK" << std::endl;
 
   close(socket);
 }
@@ -59,11 +69,11 @@ void handle_connection(int socket) {
 int main(void) {
   int server_fd = get_socket();
   struct sockaddr_in address = bind_socket(server_fd, PORT);
-  printf("bind OK!\n");
+  std::cout << "bind OK" << std::endl;
 
   while (true) {
     int new_socket = listen_and_accept_socket(server_fd, address);
-    printf("Got connection!\n");
+    std::cout << "Got connection" << std::endl;
 
     handle_connection(new_socket);
   }
--- src/server.cpp
+++ src/server.cpp
@@ -11,17 +11,20 @@ const char SERVE_DIRECTORY[] = "serve";
 
 // TODO: add more errors
 
-typedef enum { OK = 200, INTERNAL_SERVER_ERROR = 503 } server_status_code_t;
-
-struct payload_t {
-  std::string contents_own;
-  std::string content_type_own;
+enum server_status_code_t {
+  OK = 200,
+  FILE_NOT_FOUND = 404,
+  INTERNAL_SERVER_ERROR = 503
 };
 
-// TODO: change the pointer type
+struct payload_t {
+  std::string contents;
+  std::string content_type;
+};
+
 struct raw_response_t {
   server_status_code_t status_code;
-  payload_t *payload;
+  std::optional<payload_t> payload;
 };
 
 struct mime_t {
@@ -29,49 +32,51 @@ struct mime_t {
   std::string mime_type;
 };
 
-mime_t mime_types[] = {{"js", "text/javascript"},
-                       {"css", "text/css"},
-                       {"html", "text/html"},
-                       {"jpeg", "image/jpeg"}};
+std::vector<mime_t> mime_types = {{"js", "text/javascript"},
+                                  {"css", "text/css"},
+                                  {"html", "text/html"},
+                                  {"jpeg", "image/jpeg"}};
 
 std::string get_mime_type_by_filename(const std::string &filename) {
-  // TODO: handle errors if no extension
   size_t pos = filename.find('.');
+  if (pos == std::string::npos) {
+    return "UNKNOWN";
+  }
+
   std::string extension = filename.substr(pos + 1);
 
-  for (size_t i = 0; i < sizeof(mime_types) / sizeof(mime_types[0]); ++i) {
-    if (extension == mime_types[i].extension) {
-      return mime_types[i].mime_type;
+  for (auto &mime : mime_types) {
+    if (extension == mime.extension) {
+      return mime.mime_type;
     }
   }
-  return NULL;
+  return "UNKNOWN";
 }
 
-raw_response_t fetch_file(std::string requested_path) {
+raw_response_t fetch_file(const std::string &requested_path) {
   std::string filename = SERVE_DIRECTORY + std::string("/") + requested_path;
 
   // TODO: rewrite this using C++ stdlib
   FILE *stream = fopen(filename.c_str(), "r");
   if (stream == NULL) {
     fprintf(stdout, "Couldn't open file %s\n", filename.c_str());
-    raw_response_t response = {.status_code = INTERNAL_SERVER_ERROR,
-                               .payload = NULL};
+    raw_response_t response = {.status_code = FILE_NOT_FOUND,
+                               .payload = std::nullopt};
     return response;
   }
 
   std::string contents;
-
   char buf[1024 + 1];
   int n;
   while ((n = fread(buf, sizeof(buf[0]), 1024, stream)) > 0) {
-    buf[n] = '\0';
-    contents += buf;
+    contents += std::string(buf, buf + n);
   }
 
-  payload_t *payload = new payload_t;
-  payload->contents_own = contents;
-  payload->content_type_own = get_mime_type_by_filename(filename);
-  raw_response_t response = {.status_code = OK, .payload = payload};
+  payload_t payload = {.contents =
+                           std::string(contents.begin(), contents.end()),
+                       .content_type = get_mime_type_by_filename(filename)};
+  raw_response_t response = {.status_code = OK,
+                             .payload = std::optional(payload)};
   return response;
 }
 
@@ -79,6 +84,8 @@ std::string status_code_to_text(server_status_code_t status_code) {
   switch (status_code) {
   case OK:
     return "OK";
+  case FILE_NOT_FOUND:
+    return "File Not Found";
   case INTERNAL_SERVER_ERROR:
     return "Internal Server Error";
   }
@@ -86,7 +93,7 @@ std::string status_code_to_text(server_status_code_t status_code) {
 
 // TODO: path sanitanization
 
-std::string render_http_response(raw_response_t response) {
+std::string render_http_response(const raw_response_t &response) {
   std::string result;
 
   auto status_code_str = std::to_string(response.status_code);
@@ -95,20 +102,24 @@ std::string render_http_response(raw_response_t response) {
 
   result += "Server: SimpleHttpInC/0.1 C23\r\n";
 
-  if (response.payload != NULL) {
-    result += "Content-type: " + response.payload->content_type_own + "\r\n";
+  if (response.payload) {
+    result += "Content-type: " + response.payload->content_type + "\r\n";
     result += "Content-length: " +
-              std::to_string(response.payload->contents_own.length()) + "\r\n";
+              std::to_string(response.payload->contents.length()) + "\r\n";
 
     // TODO: last-modified
 
     result += "\r\n";
-    result += response.payload->contents_own;
+    result += response.payload->contents;
   }
   return result;
 }
 
 std::string access_file(const std::string &requested_path) {
   raw_response_t response = fetch_file(requested_path);
+  if (response.payload) {
+    std::cout << "response length = " << response.payload->contents.length()
+              << std::endl;
+  }
   return render_http_response(response);
 }
